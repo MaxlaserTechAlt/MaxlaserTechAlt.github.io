e2e536919acc0c76b87069b45083e3f86669f04d
MaxlaserTechAlt <135626624+MaxlaserTechAlt@users.noreply.github.com>
2023-06-07 16:05:55 +0700

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,6 +1,16 @@
 repeat task.wait() until game:IsLoaded()
 local queueteleport = queue_on_teleport
 
+if not isfolder("MaxUiX") then
+	makefolder("MaxUiX")
+end
+if not isfolder("MaxUiX/CustomModules") then
+	makefolder("MaxUiX/CustomModules")
+    end
+if not isfolder("MaxUiX/Assets") then
+	makefolder("MaxUiX/Assets")
+    end
+
 local TeleportString = [[
   loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxlaserTechAlt/MaxlaserTechAlt.github.io/main/MainScript.lua", true))
 ]]
@@ -14,20 +24,24 @@ true))()
 
 
 function MainLoaded()
-  local customModuleURL = "https://raw.githubusercontent.com/MaxlaserTechAlt/MaxlaserTechAlt.github.io/main/CustomModules/"..game.PlaceId..".lua"
-  local customModuleScript = game:HttpGet(customModuleURL, true)
-  if customModuleScript then
-    local success, error = pcall(function()
-      loadstring(customModuleScript)()
-    end)
-    if not success then
-      print("Failed To Loaded Modules: " .. tostring(error))
-      loadstring(AnyGame)()
-    end
-  else
-    loadstring(AnyGame)()
-  end
+ local customModuleURL = "https://raw.githubusercontent.com/MaxlaserTechAlt/MaxlaserTechAlt.github.io/main/CustomModules/"..game.PlaceId..".lua"
+ local customModuleScript = game:HttpGet(customModuleURL, true)
+ if customModuleScript then
+ writefile("MaxUiX/CustomModules/" .. game.PlaceId .. ".lua", customModuleScript)
+ local success, error = pcall(function()
+ loadstring(customModuleScript)()
+ end)
+ if not success then
+ print("Failed To Loaded Modules: " .. tostring(error))
+ loadstring(AnyGame)()
+ end
+ else
+ print("No custom module found for this PlaceId")
+ loadstring(AnyGame)()
+ end
 end
+
+
 
 task.spawn(function()
   MainLoaded()
