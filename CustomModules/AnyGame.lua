4da7d731d83d5383bc31710ab66897465d684df4
MaxlaserTechAlt <135626624+MaxlaserTechAlt@users.noreply.github.com>
2023-06-07 18:01:41 +0700

    Update AnyGame.lua

--- CustomModules/AnyGame.lua
+++ CustomModules/AnyGame.lua
@@ -1,4 +1,3 @@
-shared.MaxUiXAnyGameLoaded = true
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxlaserTechAlt/MaxlaserTechAlt.github.io/main/KavoCustom.lua"))()
 
 local Window = Library.CreateLib("MaxUi X | AnyGame", "Luna")
@@ -71,3 +70,91 @@ Tab1Section:NewButton("Dex V5", "gui is now cooler", function(txt)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
 end)
 
+
+if shared.MaxUiXLoaded == true then
+    print("loaded")
+else    
+    print("error")
+end
+shared.MaxUiXAnyGameLoaded = true
+if shared.GuiLibrary then
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    screenGui.ResetOnSpawn = false
+    
+    local button = Instance.new("TextButton")
+    button.Position = UDim2.new(1, -30, 0, 18)
+    button.Text = "MaxUi"
+    button.BackgroundColor3 = Color3.fromRGB(26, 25, 26)
+    button.TextColor3 = Color3.new(1, 1, 1)
+    button.Size = UDim2.new(0, 30, 0, 20)
+    button.BorderSizePixel = 0
+    button.BackgroundTransparency = 0.5
+    button.Parent = screenGui
+    button.MouseButton1Click:Connect(function()
+        Library:ToggleUI()
+    end)
+    else
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    screenGui.ResetOnSpawn = false
+    
+    local button = Instance.new("TextButton")
+    button.Position = UDim2.new(1, -30, 0, 0)
+    button.Text = "MaxUi"
+    button.BackgroundColor3 = Color3.fromRGB(26, 25, 26)
+    button.TextColor3 = Color3.new(1, 1, 1)
+    button.Size = UDim2.new(0, 30, 0, 20)
+    button.BorderSizePixel = 0
+    button.BackgroundTransparency = 0.5
+    button.Parent = screenGui
+    button.MouseButton1Click:Connect(function()
+        Library:ToggleUI()
+    end)
+    end
+    
+    while true do wait()
+    repeat task.wait() until shared.GuiLibrary
+    local screenGui = game.Players.LocalPlayer:WaitForChild("PlayerGui"):FindFirstChild("ScreenGui")
+    if screenGui then
+        screenGui:Destroy()
+    end
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    screenGui.ResetOnSpawn = false
+    
+    local button = Instance.new("TextButton")
+    button.Position = UDim2.new(1, -30, 0, 18)
+    button.Text = "MaxUi"
+    button.BackgroundColor3 = Color3.fromRGB(26, 25, 26)
+    button.TextColor3 = Color3.new(1, 1, 1)
+    button.Size = UDim2.new(0, 30, 0, 20)
+    button.BorderSizePixel = 0
+    button.BackgroundTransparency = 0.5
+    button.Parent = screenGui
+    button.MouseButton1Click:Connect(function()
+        Library:ToggleUI()
+    end)
+    
+    repeat task.wait() until not shared.GuiLibrary 
+    local screenGui = game.Players.LocalPlayer:WaitForChild("PlayerGui"):FindFirstChild("ScreenGui")
+    if screenGui then
+        screenGui:Destroy()
+    end
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    screenGui.ResetOnSpawn = false
+    
+    local button = Instance.new("TextButton")
+    button.Position = UDim2.new(1, -30, 0, 0)
+    button.Text = "MaxUi"
+    button.BackgroundColor3 = Color3.fromRGB(26, 25, 26)
+    button.TextColor3 = Color3.new(1, 1, 1)
+    button.Size = UDim2.new(0, 30, 0, 20)
+    button.BorderSizePixel = 0
+    button.BackgroundTransparency = 0.5
+    button.Parent = screenGui
+    button.MouseButton1Click:Connect(function()
+        Library:ToggleUI()
+    end)
+    end
