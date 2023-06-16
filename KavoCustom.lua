1bfcfc4fda8b16be523cb41fad70fe47bef4e43a
MaxlaserTechAlt <135626624+MaxlaserTechAlt@users.noreply.github.com>
2023-06-16 23:58:52 +0700

    Update KavoCustom.lua

--- KavoCustom.lua
+++ KavoCustom.lua
@@ -154,6 +154,88 @@ function Kavo:ToggleUI()
     end
 end
 
+function Kavo:ToggleUiButton()
+if shared.GuiLibrary then
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    
+    local button = Instance.new("TextButton")
+    button.Position = UDim2.new(1, -30, 0, 18)
+    button.Text = "MaxUi"
+    button.BackgroundColor3 = Color3.fromRGB(26, 25, 26)
+    button.TextColor3 = Color3.new(1, 1, 1)
+    button.Size = UDim2.new(0, 30, 0, 20)
+    button.BorderSizePixel = 0
+    button.BackgroundTransparency = 0.5
+    button.Parent = screenGui
+    button.MouseButton1Click:Connect(function()
+        Kavo:ToggleUI()
+    end)
+    else
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    
+    local button = Instance.new("TextButton")
+    button.Position = UDim2.new(1, -30, 0, 0)
+    button.Text = "MaxUi"
+    button.BackgroundColor3 = Color3.fromRGB(26, 25, 26)
+    button.TextColor3 = Color3.new(1, 1, 1)
+    button.Size = UDim2.new(0, 30, 0, 20)
+    button.BorderSizePixel = 0
+    button.BackgroundTransparency = 0.5
+    button.Parent = screenGui
+    button.MouseButton1Click:Connect(function()
+        Kavo:ToggleUI()
+    end)
+    end
+    
+    while true do wait()
+    repeat task.wait() until shared.GuiLibrary
+    local screenGui = game.Players.LocalPlayer:WaitForChild("PlayerGui"):FindFirstChild("ScreenGui")
+    if screenGui then
+        screenGui:Destroy()
+    end
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    
+    local button = Instance.new("TextButton")
+    button.Position = UDim2.new(1, -30, 0, 18)
+    button.Text = "MaxUi"
+    button.BackgroundColor3 = Color3.fromRGB(26, 25, 26)
+    button.TextColor3 = Color3.new(1, 1, 1)
+    button.Size = UDim2.new(0, 30, 0, 20)
+    button.BorderSizePixel = 0
+    button.BackgroundTransparency = 0.5
+    button.Parent = screenGui
+    button.MouseButton1Click:Connect(function()
+        Kavo:ToggleUI()
+    end)
+    
+    repeat task.wait() until not shared.GuiLibrary 
+    local screenGui = game.Players.LocalPlayer:WaitForChild("PlayerGui"):FindFirstChild("ScreenGui")
+    if screenGui then
+        screenGui:Destroy()
+    end
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    
+    local button = Instance.new("TextButton")
+    button.Position = UDim2.new(1, -30, 0, 0)
+    button.Text = "MaxUi"
+    button.BackgroundColor3 = Color3.fromRGB(26, 25, 26)
+    button.TextColor3 = Color3.new(1, 1, 1)
+    button.Size = UDim2.new(0, 30, 0, 20)
+    button.BorderSizePixel = 0
+    button.BackgroundTransparency = 0.5
+    button.Parent = screenGui
+    button.MouseButton1Click:Connect(function()
+        Kavo:ToggleUI()
+    end)
+    end	
+end
+
+
+
 function Kavo.CreateLib(kavName, themeList)
     if not themeList then
         themeList = themes
